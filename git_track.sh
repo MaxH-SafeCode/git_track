3f77ce13070476c85cb06f67af1b9fda2888ea9a
MaxH-SafeCode <swarchitect@planetmax.net>
2025-04-23 15:23:45 -0500

    Update git_track.sh - Added short hash

--- git_track.sh
+++ git_track.sh
@@ -4,6 +4,7 @@
 f="git_version.h"
 echo Creating $f
 hash=$(git rev-parse HEAD)
+hash_short=$(git rev-parse HEAD | sed -z 's/^\(.\{8\}\).*$/\1/')
 date=$(git show -s --format=%ci)
 dirty="$(git status --short | head -n1 | sed 's/^.*[^ ]*$/-modified/')"
 
@@ -12,9 +13,11 @@ echo \#define __GIT_VERSION_H__ >> $f
 echo \/\* THIS FILE IS AUTO GENERATED \*\/ >> $f
 echo \/\* DO NOT TRACK THIS FILE WITH THE VCS \*\/ >> $f
 echo \/\* GIT_TRACK_DIRTY can be used to indicate that status is not clean \*\/
-echo \#define GIT_TRACK_DATE \"$date\" >> $f
-echo \#define GIT_TRACK_HASH \"$hash\" >> $f
-echo \#define GIT_TRACK_DIRTY \"$dirty\" >> $f
+echo \#define GIT_TRACK_DATE \"${date}\" >> $f
+echo \#define GIT_TRACK_HASH \"${hash}\" >> $f
+echo \#define GIT_TRACK_SHORT_HASH \"${hash_short}\" >> $f
+echo \#define GIT_TRACK_DIRTY \"${dirty}\" >> $f
+echo \#define GIT_VERSION_HASH_NUMERIC \"0x${hash_short}\" >> $f
 echo \#endif >> $f
 
 ##############################################################################
