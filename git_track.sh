f7b02013ba9ca566b00d7821857c3cada28a4f60
MaxH-SafeCode <swarchitect@planetmax.net>
2025-04-23 14:04:01 -0500

    Update and rename vcs_track.sh to git_track.sh
    Added dirty attribute as indication that changes have been made to the checkout.

---
+++ git_track.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##############################################################################
+f="git_version.h"
+echo Creating $f
+hash=$(git rev-parse HEAD)
+date=$(git show -s --format=%ci)
+dirty="$(git status --short | head -n1 | sed 's/^.*[^ ]*$/dirty/')"
+
+echo \#ifndef __GIT_VERSION_H__ > $f
+echo \#define __GIT_VERSION_H__ >> $f
+echo \/\* THIS FILE IS AUTO GENERATED \*\/ >> $f
+echo \/\* DO NOT TRACK THIS FILE WITH THE VCS \*\/ >> $f
+echo \/\* GIT_TRACK_DIRTY can be used to indicate that status is not clean \*\/
+echo \#define GIT_TRACK_DATE \"$date\" >> $f
+echo \#define GIT_TRACK_HASH \"$hash\" >> $f
+echo \#define GIT_TRACK_DIRTY \" $dirty \" >> $f
+echo \#endif >> $f
+
+##############################################################################
+#Future
+#Get SW commit data
+#Get HW commit data
+#Get HW/IP commit data  ==> Recursively and generate a super string?
