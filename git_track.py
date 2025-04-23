4bbf5665fbfa8dc122f507b7901e7661f3d54a59
MaxH-SafeCode <swarchitect@planetmax.net>
2025-04-23 13:18:21 -0500

    Update and rename vcs_track.py to git_track.py
    Changed "vcs" to "git" to allow for expansion to other version control systems.

--- git_track.py
+++ git_track.py
@@ -4,7 +4,7 @@ import subprocess
 #This script generates a c header with VCS tracking information (commit hash
 #and date).
 ##############################################################################
-print "Starting VCS_TRACK"
+print "Starting GIT_TRACK"
 proc = subprocess.Popen(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE, shell=True)
 #proc = subprocess.Popen(["git", "show", "-s", "--format=%H"], stdout=subprocess.PIPE, shell=True)
 (a, err) = proc.communicate()
@@ -13,15 +13,15 @@ proc = subprocess.Popen(["git", "show", "-s", "--format=%ci"], stdout=subprocess
 hash_info = a.strip("\n\r")
 date_info = b.strip("\n\r")
 
-print "Creating vcs_track.h"
+print "Creating git_version.h"
 print b, a
-f=open("vcs_track.h","w")
-f.write("#ifndef __VCS_TRACK_H__\n")
-f.write("#define __VCS_TRACK_H__\n")
-f.write("//THIS FILE IS AUTO GENERATED\n")
-f.write("//DO NOT TRACK THIS FILE WITH THE VCS\n")
-f.write("#define VCS_TRACK_DATE " + "\"" + date_info + "\"\n")
-f.write("#define VCS_TRACK_HASH " + "\"" + hash_info.upper() + "\"\n")
+f=open("git_version.h","w")
+f.write("#ifndef __GIT_VERSION_H__\n")
+f.write("#define __GIT_VERSION_H__\n")
+f.write("/* THIS FILE IS AUTO GENERATED */\n")
+f.write("/* DO NOT TRACK THIS FILE WITH THE VCS */\n")
+f.write("#define GIT_TRACK_DATE " + "\"" + date_info + "\"\n")
+f.write("#define GIT_TRACK_HASH " + "\"" + hash_info.upper() + "\"\n")
 f.write("#endif\n")
 
 
